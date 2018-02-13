618a01aaf2d671059f31c9a46610d0cefb20ee6f
Alison Wuerfel <awuerf4505@greenvilleschools.us>
2018-02-13 13:45:51 -0500

    Add files via upload

---
+++ substitution_cipher.py
@@ -0,0 +1,33 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz "
+key = "qwertyuioplkjhgfdsazxcvbnm "
+
+def encrypt(message):
+    result = ""
+
+    for letter in message:
+        loc = alphabet.find(letter)
+        result += key[loc]
+
+    return result
+
+def decrypt(message):
+    result = ""
+    for letter in message:
+        loc = key.find(letter)
+        result += alphabet[loc]
+
+    return result
+
+
+unencrypted_message = "encryption is fun"
+encrypted_message = encrypt(unencrypted_message)
+decrypted_message = decrypt(encrypted_message)
+
+print(unencrypted_message)
+print(encrypted_message)
+print(decrypted_message)
+
+with open('substitution_sample_run.txt', 'w') as f:
+    f.write(encrypted_message + "\n")
+    f.write(decrypt(encrypted_message))
+            
